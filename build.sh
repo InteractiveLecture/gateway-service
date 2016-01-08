609059be2dfcd1fecc6a7130a94c89705a6a18a3
Rene Richter <Richterrettich@gmail.com>
2016-01-08 11:26:56 +0100

    Add to CI

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker build -t gateway-service .
+docker build -t openservice/gateway-service .
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/gateway-service" ] ; then
+  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" -e="$DOCKER_EMAIL"
+  docker push openservice/gateway-service:latest
+fi
